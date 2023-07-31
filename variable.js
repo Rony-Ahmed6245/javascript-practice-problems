f151ae04ca8067b9371588ee5947d05a779e5391
ronyahmed6245 <ronyahmed6245.gmail.com>
2023-07-31 23:38:02 +0600

    basic js

--- variable.js
+++ variable.js
@@ -1,2 +1,112 @@
-var laptopPrise = 40000 ;
-console.log(laptopPrise)
+// var laptopPrise = 40000 ;
+// console.log(laptopPrise)
+// var number = 10 ;
+// var isExpensive = true ;
+// console.log(isExpensive)
+// var friendsName = ["rony", "shakib", "salim", "foysal "]
+
+// var result =" 4.74" ;
+// var result = parseFloat(result);
+// var results = result.toFixed(2);
+// results = parseFloat(result)
+// console.log(results)
+
+
+// var num1 = "3.37676"; 
+// num2 =parseFloat(num1);
+// num2 = num2.toFixed(2);
+// num2 = parseFloat(num2);
+// console.log(typeof num2)
+
+
+// var numbers = [ 29, 34, 43,  39 , 23, 98, 45];
+// console.log(numbers)
+// // console.log( numbers.length);
+// numbers[3] = 2900 ;
+// console.log( numbers);
+
+
+// var rolNum = [ 2, 4 , 5, 8, 23, 84, 39, 24, 43, 9, 90];
+// rolNum.push(100);
+// console.log(rolNum);
+// console.log(rolNum)
+// rolNum.pop()
+// console.log(rolNum);
+// rolNum.shift();
+// rolNum.unshift(3333)
+// console.log(rolNum);
+// rolNum.push(299);
+// console.log(rolNum)
+// rolNum.pop();
+// console.log(rolNum)
+
+
+// var myHome = ["computer","laptop", "pen", "fan", "light" ];
+// var myHome = myHome.indexOf("light")
+// console.log(myHome);
+
+
+// loop 
+
+// var myMoney = 300;
+// var eggPrise = 450; 
+// if ( myMoney >= eggPrise ){
+//     console.log( "i well buying the egg")
+// }
+// else{
+//     console.log("insuffecence blance")
+// }
+
+
+
+
+
+// var totalScore =33;
+
+// if( totalScore >= 80){
+//     console.log( "congratulations...!  you'r passed A+")
+// }
+// else if( totalScore >= 70 ) {
+//     console.log("you'r passed A")
+// }
+// else if( totalScore >= 60 ) {
+//     console.log("you'r passed A-")
+// }
+// else if( totalScore >= 50 ) {
+//     console.log("you'r passed b")
+// }
+// else if( totalScore >= 40 ) {
+//     console.log("you'r passed c")
+// }
+// else if( totalScore >= 33 ) {
+//     console.log("you'r passed d")
+// }
+// else {
+//     console.log("you'r fail ")
+// }
+
+// var i =1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// var i =5;
+// while( i <= 10 ){
+//     console.log("hello");
+//     i++;
+// }
+
+// for(i = 0; i <= 10; i+=2 ){
+//     console.log(i)
+// }
+
+
+
+
+//basic  problem solving 
+
+
+// 1...problem
+
+
